e78b84cedadd83bb811d9a2f797819a8d53e198a
mavlon <mavlon>
2024-12-05 18:04:36 +0500

    change youtube video

--- src/components/AboutVideo.jsx
+++ src/components/AboutVideo.jsx
@@ -5,7 +5,7 @@ export default function AboutVideo() {
         className="m-auto w-full rounded-xl  sm:h-[295px] sm:max-w-[620px] md:h-[395px] md:max-w-[720px] lg:h-[500px] lg:max-w-[1000px] lg:rounded-[42px] xl:h-[636px] xl:max-w-[1154px]"
         width="354"
         height="195"
-        src="https://www.youtube.com/embed/HFGBUCjtmlc?si=WFF-F7pkA1fkIj9j"
+        src="https://www.youtube.com/embed/Zu00I_pfFYI?si=MaEZSVTwreJxYWvw"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
